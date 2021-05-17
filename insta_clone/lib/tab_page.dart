03b2c8d5b297b3297ef1b978123a9ece3857d99f
jeeHwon <jeehwon01@gmail.com>
2021-05-17 12:41:18 +0900

    ADD 0517 study update insta_clone

--- insta_clone/lib/tab_page.dart
+++ insta_clone/lib/tab_page.dart
@@ -8,10 +8,37 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('tab page'),
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTabbed,
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text('Search')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text('Account_circle')),
+          ]),
     );
+  }
+
+  void _onItemTabbed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
